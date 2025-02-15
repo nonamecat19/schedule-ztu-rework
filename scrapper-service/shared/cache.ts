f11bcb90272354d8633c571cf03fb8865b853c8b
Shumsky Alexander <vt211_shov@student.ztu.edu.ua>
2025-02-15 23:10:15 +0200

    feat: migrate from ioredis to node-redis

--- scrapper-service/shared/cache.ts
+++ scrapper-service/shared/cache.ts
@@ -1,7 +1,9 @@
-import Redis from "ioredis";
 import "dotenv/config"
+import {createClient} from "redis";
 
-const redis = new Redis({password: process.env.REDIS_PASSWORD});
+const redis = await createClient({
+    url: process.env.REDIS_URL,
+}).connect();
 
 type CacheKey = `${string}:${string}`;
 
@@ -18,7 +20,7 @@ export async function readCache(key: CacheKey) {
 export async function setCache(key: CacheKey, value: unknown, ttl = 3600) {
     try {
         const stringValue = typeof value === "string" ? value : JSON.stringify(value);
-        await redis.set(key, stringValue, "EX", ttl);
+        await redis.set(key, stringValue, {EX: ttl});
     } catch (error) {
         console.error("Redis SET error:", error);
     }
